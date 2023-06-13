ecded45fea76e1bee44852b82a51f0f3a8165aac
dkdkdkdk555 <dkdkdkdk555@naver.com>
2023-06-13 17:12:57 +0900

    first commit

---
+++ src/main/java/com/nurse/school/NurschoolApplication.java
@@ -0,0 +1,13 @@
+package com.nurse.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nurse/school/NurschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurse.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
