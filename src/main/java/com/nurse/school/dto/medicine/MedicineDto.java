4b266a13a8c23a6d545247a6d50e7361676c6465
dkdkdkdk555 <dkdkdkdk555@naver.com>
2023-09-06 21:30:17 +0900

    약품 재고 조회 수정
    dto에 id 도 modelMapper에서 dto로 변환 가능하도록 생성자 인자에 추가

--- src/main/java/com/nurse/school/dto/medicine/MedicineDto.java
+++ src/main/java/com/nurse/school/dto/medicine/MedicineDto.java
@@ -2,6 +2,7 @@ package com.nurse.school.dto.medicine;
 
 import com.nurse.school.entity.Medicine;
 import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class MedicineDto {
 
     @NotNull
@@ -32,6 +34,7 @@ public class MedicineDto {
 
     public MedicineDto(Long schoolId, String medicine_name, String usage, String unit, int quantity, int capa, int page) {
         this.schoolId = schoolId;
+//        this.id = id;
         this.medicine_name = medicine_name;
         this.usage = usage;
         this.unit = unit;
